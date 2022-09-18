c148c6824dac1c364f5793eff35e047c0e8f66c3
Chandra631 <lokchandra31@gmail.com>
2022-09-18 21:10:34 +0530

    680

--- abc.py
+++ abc.py
@@ -134,3 +134,17 @@ class Solution:
         """
         node.val=node.next.val
         node.next=node.next.next
+
+
+##################### 680
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l=0
+        r=len(s)-1
+        while l<r:
+            if s[l]!=s[r]:
+                skipL,skipR=s[l+1:r+1],s[l:r]
+                return (skipL==skipL[::-1]) or (skipR==skipR[::-1])
+            l=l+1
+            r=r-1
+        return True
