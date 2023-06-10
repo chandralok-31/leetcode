09ec504e0cd2a69a615bdca3c8f00d556718043e
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-10 13:26:29 +0530

    Time: 2735 ms (71.34%), Space: 21.2 MB (38.85%) - LeetHub

--- 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
+++ 2249-count-lattice-points-inside-a-circle/2249-count-lattice-points-inside-a-circle.py
@@ -1,22 +1,22 @@
 class Solution:
     def countLatticePoints(self, circles: List[List[int]]) -> int:
         
-        res = set()
+        arr = set()
     
         def point(x, y, r):
-            nonlocal res
+            
 
             for a in range(x-r, x+r+1):
                 for b in range(y-r, y+r+1):
                     if (a-x)*(a-x) + (b-y)*(b-y) <= r*r:
-                        res.add((a,b))
+                        arr.add((a,b))
 
-            return res
+            return arr
         
         for x in circles:
 
             point(x[0],x[1],x[2])
 
-        return len(res)
+        return len(arr)
 
         
