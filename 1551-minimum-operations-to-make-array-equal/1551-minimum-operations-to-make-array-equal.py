39cc88e208f6d7030ec3c25e8526a7fe8c1ccd59
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-20 09:05:26 +0530

    Time: 155 ms (16.00%), Space: 16.6 MB (12.50%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        arr=[(2*i+1) for i in range(n)]
+        target=sum(arr)//n
+        cnt=0
+        for i in range(n//2):
+            cnt+=target-arr[i]
+        return cnt
+            
+        
+        
+        
+        
+        
+        
+        
+#         arr=[(2*i+1) for i in range(n)]
+#         target=sum(arr)//n
+#         i=0
+#         j=n-1
+#         count=0
+#         while i<j:
+#             if arr[i]!=target and arr[j]!=target:
+
+#                 arr[i]+=1
+#                 arr[j]-=1
+#                 count+=1
+#             else:
+#                 i+=1
+#                 j-=1
+#         return count
+            
+        
