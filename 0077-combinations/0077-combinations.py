011800e4055d56fff601bff3d12e68c9bdf969c4
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-24 10:53:15 +0530

    Time: 84 ms (97.65%), Space: 18.3 MB (33.10%) - LeetHub

---
+++ 0077-combinations/0077-combinations.py
@@ -0,0 +1,8 @@
+class Solution:
+    import itertools
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return list(itertools.combinations(range(1,n+1),k))
+       
+    
+
+            
