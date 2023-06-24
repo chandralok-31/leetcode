da98bcaf7568926f0f19337e31e639229247e3f3
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-24 08:32:22 +0530

    Time: 45 ms (80.57%), Space: 16.5 MB (77.99%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subsets = [[]]
+        nums.sort()
         for n in nums:
             subsets += [s + [n] for s in subsets]
         return subsets
