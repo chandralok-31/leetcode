1b3d25bf0add910e61fbc666d4dfed4110819388
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-24 08:17:26 +0530

    Time: 51 ms (49.99%), Space: 16.6 MB (39.97%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subsets = [[]]
+        for n in nums:
+            subsets += [s + [n] for s in subsets]
+        return subsets
+                
+        
