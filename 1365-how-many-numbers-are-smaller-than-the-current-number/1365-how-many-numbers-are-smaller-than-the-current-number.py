32dfd0b549451945091d2c355bf8353fad5c7394
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-26 08:04:37 +0530

    Time: 63 ms (95.64%), Space: 16.5 MB (26.27%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,21 +1,21 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        arr=[]
-        for i in range(len(nums)):
-            count=0
-            for j in range(len(nums)):
-                if nums[j]<nums[i]:
-                    count+=1
-            arr.append(count)
-        return arr
+        # arr=[]
+        # for i in range(len(nums)):
+        #     count=0
+        #     for j in range(len(nums)):
+        #         if nums[j]<nums[i]:
+        #             count+=1
+        #     arr.append(count)
+        # return arr
                 
-#         arr={i:nums.index(i) for i in nums}
-#         print(arr)
-        
-#         arr.sort()
-#         res=[]
-#         for  i in range(len(arr)):
-#             x=arr.index(nums[i])
-#             res.append(x)
-        
-#         return res       
+        dic1 = {}
+        nums1 = sorted(nums, reverse=True)
+        print(nums)
+        print(nums1)
+        for index in range(0, len(nums1) - 1):
+            if nums1[index] == nums1[index + 1]:
+                continue
+            dic1[nums1[index]] = len(nums1) - index -1    
+        dic1[nums1[-1]] = 0
+        return([dic1[x] for x in nums])      
