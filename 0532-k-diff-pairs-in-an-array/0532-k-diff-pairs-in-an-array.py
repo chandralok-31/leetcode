94ff932f66ffee05609092d0117026f845d885b6
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-28 20:54:18 +0530

    Time: 92 ms (63.94%), Space: 18.1 MB (49.16%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -8,7 +8,7 @@ class Solution:
             if x in d: d[x]+=1
             else:      d[x]=1
         
-        if k: # k>0
+        if k>0: # k>0
             answ=sum(x+k in d for x in d.keys())
         else: # k==0
             answ=sum(k>1 for k in d.values())
