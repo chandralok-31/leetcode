9b21af8117614d50fa5026ba10abccd13c76ad67
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-06-30 16:26:02 +0530

    Time: 79 ms (5.00%), Space: 16.5 MB (13.45%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        t=list(t)
+        for i in s:
+            if i in t:
+                t.remove(i)
+        return t[0]
+        
