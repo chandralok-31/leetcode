2bc2e0ab3e65f55a65a6727f58998c64f34b483a
chandralok-31 <98259782+chandralok-31@users.noreply.github.com>
2023-07-04 21:53:18 +0530

    Time: 95 ms (75.08%), Space: 19.8 MB (48.75%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,16 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals=sorted(intervals)
+        ans=[]
+        start=intervals[0][0]
+        end=intervals[0][1]
+        for i in intervals:
+            if i[0]>end:
+                ans.append([start,end])
+                start,end=i[0],i[1]
+            else:
+                end=max(end,i[1])
+        ans.append([start,end])
+        return ans
+        
